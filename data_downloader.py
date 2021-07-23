8aa47abf2ff64d144fa467242e5c76616a8c328f
Mauro Marini <mauro.marini97@gmail.com>
2021-07-23 10:48:19 +0200

    Initial commit

---
+++ data_downloader.py
@@ -0,0 +1,9 @@
+#
+#   subito-it_query_to_pdf copyright © 2021 - all rights reserved
+#   Created at: 23/07/21
+#   By: mauromarini
+#   License: MIT
+#   Repository: https://github.com/marinimau/subito-it_query_to_pdf
+#   Credits: @marinimau (https://github.com/marinimau)
+#
+
